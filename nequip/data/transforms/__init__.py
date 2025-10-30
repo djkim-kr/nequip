64f0f0c3b6de779e9e0c87241d2e64818364ecd0
cw-tan <chuinwei.tan@gmail.com>
2025-10-30 19:07:56 -0400

    refactor `NeighborListTransform` to separate neighborlist construction and pruning based on per-edge-type cutoffs

--- nequip/data/transforms/__init__.py
+++ nequip/data/transforms/__init__.py
@@ -1,6 +1,10 @@
 # This file is a part of the `nequip` package. Please see LICENSE and README at the root for information on using it.
 from .type_mapper import ChemicalSpeciesToAtomTypeMapper
-from .neighborlist import NeighborListTransform, SortedNeighborListTransform
+from .neighborlist import (
+    NeighborListPruneTransform,
+    NeighborListTransform,
+    SortedNeighborListTransform,
+)
 from .stress_utils import (
     VirialToStressTransform,
     StressSignFlipTransform,
@@ -9,6 +13,7 @@ from .stress_utils import (
 
 __all__ = [
     "ChemicalSpeciesToAtomTypeMapper",
+    "NeighborListPruneTransform",
     "NeighborListTransform",
     "SortedNeighborListTransform",
     "VirialToStressTransform",
--- nequip/data/transforms/neighborlist.py
+++ nequip/data/transforms/neighborlist.py
@@ -28,43 +28,69 @@ class NeighborListTransform:
         self.per_edge_type_cutoff = per_edge_type_cutoff
         self.kwargs = kwargs
 
-        # set up normalizer for per-edge-type cutoffs if provided
-        self._per_edge_type = False
+        # set up pruning transform for per-edge-type cutoffs if provided
+        self._pruner = None
         if per_edge_type_cutoff is not None:
             assert type_names is not None, (
                 "`type_names` required for `per_edge_type_cutoff`"
             )
-            self._per_edge_type = True
-            from nequip.nn.embedding import EdgeLengthNormalizer
-
-            self._normalizer = EdgeLengthNormalizer(
+            self._pruner = NeighborListPruneTransform(
                 r_max=self.r_max,
-                type_names=type_names,
                 per_edge_type_cutoff=per_edge_type_cutoff,
+                type_names=type_names,
             )
 
     def __call__(self, data: AtomicDataDict.Type) -> AtomicDataDict.Type:
         data = compute_neighborlist_(data, self.r_max, **self.kwargs)
 
         # prune based on per-edge-type cutoffs if specified
-        if self._per_edge_type:
-            if AtomicDataDict.ATOM_TYPE_KEY not in data:
-                raise KeyError(
-                    f"Per-edge-type cutoffs require '{AtomicDataDict.ATOM_TYPE_KEY}' to be present in the data. "
-                    "This is likely because nequip.data.transforms.ChemicalSpeciesToAtomTypeMapper needs to be applied before "
-                    "nequip.data.transforms.NeighborListTransform. Please check your data transform order in the configuration."
-                )
-            data = self._apply_per_edge_type_cutoffs(data)
+        if self._pruner is not None:
+            data = self._pruner(data)
 
         return data
 
-    def _apply_per_edge_type_cutoffs(
-        self, data: AtomicDataDict.Type
-    ) -> AtomicDataDict.Type:
-        """Prune neighbor list based on per-edge-type cutoffs using ``EdgeLengthNormalizer``."""
+
+class NeighborListPruneTransform:
+    """Prunes a neighborlist based on per-edge-type cutoffs.
+
+    Args:
+        r_max (float): global cutoff radius
+        per_edge_type_cutoff (Dict): per-edge-type cutoffs (must be <= r_max)
+        type_names (List[str]): list of atom type names
+    """
+
+    def __init__(
+        self,
+        r_max: float,
+        per_edge_type_cutoff: Dict[str, Union[float, Dict[str, float]]],
+        type_names: List[str],
+    ):
+        self.r_max = r_max
+        self.per_edge_type_cutoff = per_edge_type_cutoff
+        self.type_names = type_names
+
+        # set up normalizer for per-edge-type cutoffs
+        from nequip.nn.embedding import EdgeLengthNormalizer
+
+        self._normalizer = EdgeLengthNormalizer(
+            r_max=self.r_max,
+            type_names=type_names,
+            per_edge_type_cutoff=per_edge_type_cutoff,
+        )
+
+    def __call__(self, data: AtomicDataDict.Type) -> AtomicDataDict.Type:
+        """Prune neighbor list based on per-edge-type cutoffs."""
+
+        if AtomicDataDict.ATOM_TYPE_KEY not in data:
+            raise KeyError(
+                f"Per-edge-type cutoffs require '{AtomicDataDict.ATOM_TYPE_KEY}' to be present in the data. "
+                "This is likely because nequip.data.transforms.ChemicalSpeciesToAtomTypeMapper needs to be applied before "
+                "neighborlist pruning. Please check your data transform order."
+            )
 
         # get mask for pruning
-        # make a shallow copy of original data dict, such that the original doens't have the tensors added by applying `self._normalizer`
+        # make a shallow copy of original data dict, such that the original
+        # doesn't have the tensors added by applying `self._normalizer`
         mask = (
             self._normalizer(data.copy())[AtomicDataDict.NORM_LENGTH_KEY].view(-1)
             <= 1.0
